5e585ff72874672868fa795e7bebe0056e592c73
Maxkrr <krivosheevmax2003@gmail.com>
2025-11-07 09:59:25 +0300

    Update code.cpp

--- 2variant/code.cpp
+++ 2variant/code.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+double power(double x, int n) {
+    if (n == 0) {
+        return 1;                  // Базовый случай: любая степень нуля равна 1
+    } else if (n % 2 == 0) {      // Проверка четности
+        double y = power(x, n / 2); // Рассчитываем половину степени
+        return y * y;              // Возвращаем квадрат получившегося значения
+    } else {
+        double y = power(x, n / 2); // Нечётный случай
+        return y * y * x;          // Добавочное умножение на x
+    }
+}
